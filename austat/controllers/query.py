f46db57e628fb3c372204c4ae75dcad785f20aa3
kwauchope <kwauchope@users.noreply.github.com>
2015-07-07 02:43:48 +1000

    can now query down to the fact level

--- austat/controllers/query.py
+++ austat/controllers/query.py
@@ -12,13 +12,20 @@ queryApp = bottle.Bottle()
 
 
 @queryApp.get('/query/<iden:int>')
-def get_qn(iden):
+@queryApp.get('/query/<iden:int>/<fact:int>')
+def get_qn(iden, fact=None):
     srcs = getsources()
 
     if iden < 0 or iden >= len(srcs):
         abort(400, 'Requested source %d but only have %d' % (iden, len(srcs)))
-
     src = srcs[iden]
+
+    if fact and (fact < 0 or fact >= len(src.getdatasets())):
+        abort(400, 'Requested facts %d but only have %d' % (iden, len(src.getdatasets())))
+    
     response.content_type = 'application/json'
     #hard coded 'difficulty' for now
-    return json.dumps(src.getrandomstat(4))
+    if fact is not None:
+        return json.dumps(src.getstat(fact,4))
+    else:
+        return json.dumps(src.getrandomstat(4))
